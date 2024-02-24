cc2d2c33e6547809cf8873659e68024211d84b4e
Shasmo <115390236+shasmo-0@users.noreply.github.com>
2024-02-24 07:41:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.openAiAPI = "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";
+global.openAiAPI = "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";
 
 
 //
